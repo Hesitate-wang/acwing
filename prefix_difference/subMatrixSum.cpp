535cb41544588461ed13aeb56214cbb64ed053a8
hesitate001 <2842698953@qq.com>
2024-09-19 01:28:39 +0800

    9/19/1:28 耻辱下机

---
+++ prefix_difference/subMatrixSum.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Admin on 2024/9/19.
+
+
+#include<iostream>
+
+const int N = 1001;
+const int M = 1001;
+int main(){
+    int n, m, q;
+    std::cin >> n >> m >> q;
+    int matrix[N][M];
+    for(int i = 0;i < n; ++i){
+        for(int j = 0; j < m; ++j){
+            std::cin >> matrix[i][j];
+        }
+    }
+
+}
