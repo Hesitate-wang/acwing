ad3c6a37f854f4201288ae36a28ca63c1a15428a
Hesitate-wang <2842698953@qq.com>
2024-10-23 17:15:49 +0800

    before two

--- set/N240.cpp
+++ set/N240.cpp
@@ -3,23 +3,17 @@
 //食物链
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
 std::vector<int> vec(50001);  //A B C 集合
-std::unordered_map<int, int> ate;   //被捕食者 捕食者
-std::unordered_map<int, int> eat;   //捕食者 被捕食者
-int inChain[50001];
+std::vector<int> dis(50001);   //到祖宗节点的距离
 
-
-int search(const int x) {
-    return x == vec[x]? x: vec[x] = search(vec[x]);
-}
-void connect(int a, int b) {
-    int x = vec[a];
-    int y = vec[b];
-    if(x!=y) {
-        vec[y] = x;
+int search(int x) {
+    if(vec[x] != x) {
+        int nex = vec[x];
+        vec[x] = search(nex);
+        dis[x] = (dis[x] + dis[nex]) % 3;
     }
+    return vec[x];
 }
 
 int main() {
@@ -27,78 +21,38 @@ int main() {
     std::cin >> N >> K;
     for(int i = 0;i < 50001; ++i) {
         vec[i] = i;
-        inChain[i] = 0;
+        dis[i] = 0;
     }
-    while(K --) {
-        int D, X, Y;
-        std::cin >> D >> X >> Y;
-        if(X > N || Y > N) {
+    while(K--) {
+        int op, a, b;
+        std::cin >> op >> a >> b;
+        if( a > N || b > N) {
             ++ans;
             continue;
         }
-        if(D == 1) {
-            //X Y 是同类 检查是否冲突
-            int x = search(X);
-            int y = search(Y);
-            if(inChain[X] && inChain[Y] && ((eat[x] && eat[x] == y)
-                || (eat[y] && eat[y] == x))) {
-                //如果X Y 已经加入食物链 且确定x->y or x->z-y(y->x) 则不是同类
+        int x = search(a);
+        int y = search(b);
+        if(op == 1) {
+            // a == b
+            if(x == y && (dis[a] - dis[b] + 3) % 3 != 0) {
                 ++ans;
-            }else {
-                //真话
-                inChain[X] = inChain[Y] = 1;
-                connect(x, y);
-
-                //食物链关系转移
-                if(eat[y] && eat[x]) {
-                    connect(eat[x], eat[y]);
-                }else if(eat[y]) {
-                    eat[x] = eat[y];
-                }
-                if(ate[x] && ate[y]) {
-                    connect(ate[x], ate[y]);
-                }else if(ate[y]) {
-                    ate[x] = ate[y];
-                }
+            }else{
+                vec[x] = y;
+                dis[x] = dis[b] - dis[a];
             }
         }else {
-            //X 吃 Y 检查是否冲突
-            int x = search(X);
-            int y = search(Y);
-            if(x == y ||
-                (inChain[X] && inChain[Y] &&
-                        eat[y] && eat[y] == x)) {
-                //如果已经加入食物链 且明确x->z->y or y -> x 则假话
+            // a -> b
+            if(a == b) {
                 ++ans;
-            }else {
-                //建立捕食关系
-                if(!inChain[X] && !inChain[Y]) {
-                    ate[y] = x;
-                    eat[x] = y;
-                    inChain[X] = inChain[Y] = 1;
-                }else if(!inChain[X]) {
-                    inChain[X] = 1;
-                    if(ate[y]) {
-                        connect(x, ate[y]);
-                    }else {
-                        ate[y] = x;
-                        eat[x] = y;
-                    }
-                }else if(!inChain[Y]) {
-                    inChain[Y] = 1;
-                    if(eat[x]) {
-                        connect(eat[x], y);
-                    }else {
-                        eat[x] = y;
-                        ate[y] = x;
-                    }
-                }else {
-                    ate[y] = x;
-                    eat[x] = y;
-                }
+                continue;
+            }
+            if(x == y && (dis[a] - dis[b] + 3) % 3 != 1) {
+                ++ans;
+            }else{
+                vec[x] = y;
+                dis[x] = dis[b] - dis[a] + 1;
             }
         }
-
     }
     std::cout << ans <<std::endl;
 }
